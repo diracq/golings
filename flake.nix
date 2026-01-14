37141e8a333d5a5443144c3bf49e261b29f8f286
drew-dirac <drew@diracinc.com>
2026-01-14 17:21:17 -0500

    add install for `golings` to flake

--- flake.nix
+++ flake.nix
@@ -15,11 +15,20 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+        golings = pkgs.buildGoModule {
+          pname = "golings";
+          version = "0.0.0-dev";
+          src = ./.;
+          subPackages = [ "golings" ];
+          vendorHash = "sha256-l7DjKhUNJumHpRiA0fMlNC2B1oqG7LoIv5hjJwA4O88=";
+        };
       in
       {
+        packages.default = golings;
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             go
+            golings
           ];
         };
       }
